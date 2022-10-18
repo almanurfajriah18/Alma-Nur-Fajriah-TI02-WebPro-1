a4c68fd62ebafe42b2c2b4f9af31c244cde13ef5
almanurfajriah18 <almanurfajriah2022@gmail.com>
2022-10-18 11:43:45 +0700

    ADD Pratikum 4

---
+++ OPEN EDITORS/hello.py
@@ -0,0 +1,7 @@
+msg="Hello World"
+print(msg)
+print("Biodata Saya")
+print("NIM : 0110222222")
+print("Nama : Alma Nur Fajriah")
+print("Kelas : TI02")
+print("Alamat : Lingkungan 02 Citatah dalam RT01/13 Ciriung Cibinong Bogor")
