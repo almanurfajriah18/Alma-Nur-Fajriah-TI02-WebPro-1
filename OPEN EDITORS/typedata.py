2e4d5e7fee504ec2ca485dfba49590a1177931d6
almanurfajriah18 <almanurfajriah2022@gmail.com>
2022-10-20 14:41:58 +0700

    EDIT 4.3 PROFILE

---
+++ OPEN EDITORS/typedata.py
@@ -0,0 +1,27 @@
+#SOAL NO. 1
+NamaDepan = "Alma" #PascalCase
+NamaBelakang = "Nur Fajriah" #CamelCase
+NamaLengkap = "Alma Nur Fajriah" #SnakeCase
+Alamat = '''Lingkungan 02 Citatah Dalam Rt01/13,
+Kel. Ciriung, Kec. Cibinong, Kab.Bogor'''
+Umur = "19 Tahun"
+NoTelp = int(62895375638518)
+Tinggi = "160Cm"
+Hobi = "Makan"
+CitaCita = "Kurus"
+
+
+print(NamaDepan, type(NamaDepan))
+print(NamaBelakang)
+print(NamaLengkap)
+print(Alamat)
+print(Umur)
+print(NoTelp, type(NoTelp))
+print(Tinggi)
+print(Hobi)
+print(CitaCita)
+
+
+#SOAL NO. 2
+print(19>15) #umur
+print(160<170) #tinggi
